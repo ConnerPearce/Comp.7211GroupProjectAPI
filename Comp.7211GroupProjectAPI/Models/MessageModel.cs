1abbd3a4e12331429a5e92f462f06ea6d74f65ef
Kawhina Hohepa <27037080@student.toiohomai.ac.nz>
2019-10-10 15:55:21 +1300

    Made 2 models, messagemodel and postmodel

---
+++ Comp.7211GroupProjectAPI/Models/MessageModel.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp._7211GroupProjectAPI.Models
+{
+    public class MessageModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string Message { get; set; }
+        public int SenderID { get; set; }
+        public int DestinationID { get; set; }
+    }
+}
---
+++ Comp.7211GroupProjectAPI/Models/PostModel.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp._7211GroupProjectAPI.Models
+{
+    public class PostModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public int UId { get; set; }
+        public string PostDescription { get; set; }
+        public int UpVote { get; set; }
+        public int DownVote { get; set; }
+    }
+}
