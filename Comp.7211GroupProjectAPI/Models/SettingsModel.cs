3413322752e732d2cf1d65b2eaf2c1115dfdb3a0
Conner Pearce <metalichking@outlook.com>
2019-10-10 15:13:00 +1300

    Updated class to contain ID Numbers

--- Comp.7211GroupProjectAPI/Models/SettingsModel.cs
+++ Comp.7211GroupProjectAPI/Models/SettingsModel.cs
@@ -8,6 +8,8 @@ namespace Comp._7211GroupProjectAPI.Models
     public class SettingsModel
     {
         [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public int UId { get; set; }
         public string AppTheme { get; set; }
     }
 }
