9679038b9bde6a76c0c803b03dcf273fb862d87a
Conner Pearce <metalichking@outlook.com>
2019-10-10 16:20:46 +1300

    Updated So API works with SQLite

---
+++ Comp.7211GroupProjectAPI/SQLiteContext.cs
@@ -0,0 +1,20 @@
+﻿using Comp._7211GroupProjectAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comp._7211GroupProjectAPI
+{
+    public class SQLiteContext : DbContext
+    {
+        public SQLiteContext(DbContextOptions<SQLiteContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
--- Comp.7211GroupProjectAPI/Startup.cs
+++ Comp.7211GroupProjectAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 namespace Comp._7211GroupProjectAPI
 {
@@ -24,6 +25,7 @@ namespace Comp._7211GroupProjectAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SQLiteContext>(opt => opt.UseInMemoryDatabase("Users"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
