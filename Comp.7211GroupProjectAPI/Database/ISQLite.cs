79bf3c4c7ebbead65ac8010848d014014dc171c3
Kawhina Hohepa <27037080@student.toiohomai.ac.nz>
2019-10-10 14:46:11 +1300

    Created database and established connection, also created a usermodel.

---
+++ Comp.7211GroupProjectAPI/Database/ISQLite.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp._7211GroupProjectAPI.Database
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+    }
+}
---
+++ Comp.7211GroupProjectAPI/Database/SQLite.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Comp._7211GroupProjectAPI.Database
+{
+    public class SQLite : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var db = "AppDatabase";
+            var path = Path.Combine(System.Environment.GetFolderPath
+                (System.Environment.SpecialFolder.Personal), db);
+
+            return new SQLiteConnection(path);
+        }
+    }
+}
