c6045cfe364133d8a4690263b1898e3ef8a84564
Kawhina Hohepa <27037080@student.toiohomai.ac.nz>
2019-10-10 15:11:31 +1300

    Added settings model class

---
+++ Comp.7211GroupProjectAPI/Models/SettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp._7211GroupProjectAPI.Models
+{
+    public class SettingsModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public string AppTheme { get; set; }
+    }
+}
