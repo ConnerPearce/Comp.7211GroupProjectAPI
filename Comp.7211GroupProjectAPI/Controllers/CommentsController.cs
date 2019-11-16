2921cdaaff94669b85920756a1ac94c0058c208c
Conner Pearce <metalichking@outlook.com>
2019-11-17 09:31:45 +1300

    Updated Comments Controller and made minor adjustments to all models

--- Comp.7211GroupProjectAPI/Controllers/CommentsController.cs
+++ Comp.7211GroupProjectAPI/Controllers/CommentsController.cs
@@ -21,13 +21,6 @@ namespace Comp._7211GroupProjectAPI.Controllers
             _context = context;
         }
 
-        // GET: api/Comments
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Comments>>> GetComments()
-        {
-            return await _context.Comments.ToListAsync();
-        }
-
         // GET: api/Comments/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Comments>> GetComments(int id)
@@ -42,34 +35,15 @@ namespace Comp._7211GroupProjectAPI.Controllers
             return comments;
         }
 
-        // PUT: api/Comments/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutComments(int id, Comments comments)
+        // GET: api/Comments/postID=2
+        [HttpGet("postID={id})")]
+        public async Task<ActionResult<IEnumerable<Comments>>> GetCommentsBypost(int id)
         {
-            if (id != comments.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(comments).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CommentsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            var commentsOnPost = await _context.Comments.Where(e => e.PostId == id).ToListAsync();
+            if (commentsOnPost != null)
+                return Ok(commentsOnPost);
+            else
+                return NotFound();
         }
 
         // POST: api/Comments
@@ -96,11 +70,6 @@ namespace Comp._7211GroupProjectAPI.Controllers
             await _context.SaveChangesAsync();
 
             return comments;
-        }
-
-        private bool CommentsExists(int id)
-        {
-            return _context.Comments.Any(e => e.Id == id);
         }
     }
 }
